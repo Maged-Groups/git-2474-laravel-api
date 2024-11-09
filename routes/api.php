4e56a5bb7f6367e09cf043d25e1dd7287c97a219
Maged Yaseen <magedyaseengroups@gmail.com>
2024-11-09 23:20:08 +0200

    Day 2

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(UserController::class)
+    ->prefix('users')
+    ->group(function () {
+        Route::get('role/{role}', 'role');
+    });
+
+Route::apiResource('users', UserController::class);
+
+Route::apiResource('posts', PostController::class);
